9179cd33e52f2d29f3a3b92e68a919f718ef97dc
李兴坤 <lixingkun@lixingkundeMacBook-Pro.local>
2017-12-01 16:52:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# android_rmtp_nginx
---
+++ app/src/main/obj/local/armeabi/objs/nginx/Java_org_nginx_Nginx.o.d
@@ -0,0 +1,19 @@
+./obj/local/armeabi/objs/nginx/Java_org_nginx_Nginx.o: \
+  jni/Java_org_nginx_Nginx.cpp \
+  /Users/lixingkun/red5/crtmpserver/Cross-Compile-Nginx-with-RTMP-Module-for-Android/nginx-1.12.0///src/core/ngx_config.h \
+  jni/ngx_auto_headers.h \
+  /Users/lixingkun/red5/crtmpserver/Cross-Compile-Nginx-with-RTMP-Module-for-Android/nginx-1.12.0///src/os/unix/ngx_posix_config.h \
+  /Users/lixingkun/red5/crtmpserver/Cross-Compile-Nginx-with-RTMP-Module-for-Android/nginx-1.12.0///src/os/unix/glob.h \
+  jni/ngx_auto_config.h jni/nginx.h
+
+/Users/lixingkun/red5/crtmpserver/Cross-Compile-Nginx-with-RTMP-Module-for-Android/nginx-1.12.0///src/core/ngx_config.h:
+
+jni/ngx_auto_headers.h:
+
+/Users/lixingkun/red5/crtmpserver/Cross-Compile-Nginx-with-RTMP-Module-for-Android/nginx-1.12.0///src/os/unix/ngx_posix_config.h:
+
+/Users/lixingkun/red5/crtmpserver/Cross-Compile-Nginx-with-RTMP-Module-for-Android/nginx-1.12.0///src/os/unix/glob.h:
+
+jni/ngx_auto_config.h:
+
+jni/nginx.h:
